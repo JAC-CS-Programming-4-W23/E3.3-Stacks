dc6b4927a7ef1836172cbee05eb0262de4921fca
Vikram Singh <vikram.singh@johnabbott.qc.ca>
2023-02-19 14:57:15 -0500

    Added solution.

--- src/IntStack.java
+++ src/IntStack.java
@@ -1,3 +1,37 @@
 public class IntStack {
 
+    private int[] elements;
+    private int top;
+
+    public IntStack(int cap) {
+        elements = new int[cap];
+        top = -1;
+    }
+
+    public void push(int element) {
+        if (isFull()) //(top == elements.length - 1)
+            throw new StackOverflowException();
+        elements[++top] = element;
+    }
+
+    public int pop() {
+        if (isEmpty())  //(top == -1)
+            throw new StackUnderflowException();
+        return elements[top--];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == elements.length - 1;
+    }
+
+    public int top() {
+        if (isEmpty())
+            throw new StackUnderflowException();
+        return elements[top];
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,29 @@ public class Main {
     }
 
     public static boolean balanced(String exp) {
-        return false;
+        IntStack brackets = new IntStack(exp.length()/2 + 1);
+
+        for (char c : exp.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                if (brackets.isFull())  // because the stack is 1/2 of the length of the string
+                                        // if it's full then it's impossible to have enough close
+                    return false;
+                brackets.push(c);
+            }
+            else {
+                if (brackets.isEmpty())
+                    return false;
+                char tmp = (char) brackets.pop();
+                if (c == ')' && tmp != '(')
+                    return false;
+                if (c == '}' && tmp != '{')
+                    return false;
+                if (c == ']' && tmp != '[')
+                    return false;
+            }
+        }
+		
+        return brackets.isEmpty();
     }
 
 }
